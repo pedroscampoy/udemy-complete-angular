a03cde89ee9476c555c2f158bd128e195c687628
Laura <pedroscampoy@gmail.com>
2021-07-13 16:42:18 +0200

    recipe list html

---
+++ src/app/components/recipes/recipe.model.ts
@@ -0,0 +1,17 @@
+// export interface Recipe {
+//   name: string;
+//   description: string;
+//   imagePath: string;
+// }
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
